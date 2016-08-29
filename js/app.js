e0cd45f75e244e46f754992f67787bb4382fb731
Oliver Barnum (RIT Student) <oeb9106@ad.rit.edu>
2016-08-29 13:52:22 -0400

    Remove stub and add basic phaser game...

--- js/app.js
+++ js/app.js
@@ -1,196 +1,40 @@
+var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
-window.onload = function() {
+function preload() {
+    // game.load.image('sky', 'assets/sky.png');
+}
 
-    // A cross-browser requestAnimationFrame
-    // See https://hacks.mozilla.org/2011/08/animating-with-javascript-from-setinterval-to-requestanimationframe/
-    var requestAnimFrame = (function() {
-        return window.requestAnimationFrame    ||
-            window.webkitRequestAnimationFrame ||
-            window.mozRequestAnimationFrame    ||
-            window.oRequestAnimationFrame      ||
-            window.msRequestAnimationFrame     ||
-            function(callback){
-                window.setTimeout(callback, 1000 / 60);
-            };
-    })();
+function create() {
+    
+    game.physics.startSystem(Phaser.Physics.ARCADE);
 
-    // Install logic
-    // If the app has already been installed, we don't do anything.
-    // Otherwise we'll show the button, and hide it when/if the user installs the app.
-    var installButton = document.getElementById('install');
-    var manifestPath = AppInstall.guessManifestPath();
+    //  A simple background for our game
+    game.add.sprite(0, 0, 'sky');
 
-    if(AppInstall.isInstallable()) {
+    //  The platforms group contains the ground and the 2 ledges we can jump on
+    platforms = game.add.group();
 
-      // checking for app installed is an asynchronous process
-      AppInstall.isInstalled(manifestPath, function isInstalledCb(err, result) {
+    //  We will enable physics for any object that is created in this group
+    platforms.enableBody = true;
 
-        if(!err && !result) {
+    // Here we create the ground.
+    var ground = platforms.create(0, game.world.height - 64, 'ground');
 
-          // No errors, and the app is not installed, so we can show the install button,
-          // and set up the click handler as well.
-          installButton.classList.remove('hidden');
+    //  Scale it to fit the width of the game (the original sprite is 400x32 in size)
+    ground.scale.setTo(2, 2);
 
-          installButton.addEventListener('click', function() {
+    //  This stops it from falling away when you jump on it
+    ground.body.immovable = true;
 
-            AppInstall.install(manifestPath, function(err) {
-              if(!err) {
-                installButton.classList.add('hidden');
-              } else {
-                alert('The app cannot be installed: ' + err);
-              }
-            });
+    //  Now let's create two ledges
+    var ledge = platforms.create(400, 400, 'ground');
 
-          }, false);
+    ledge.body.immovable = true;
 
-        }
+    ledge = platforms.create(-150, 250, 'ground');
 
-      });
+    ledge.body.immovable = true;
+}
 
-    }
-
-
-    // Create the canvas
-    var mainContainer = document.querySelector('main');
-    var canvas = document.createElement("canvas");
-    var infoContainer = document.querySelector('div.info');
-    var ctx = canvas.getContext("2d");
-    var initialCanvasWidth = canvas.width = 320;
-    var initialCanvasHeight = canvas.height = 480;
-    mainContainer.appendChild(canvas);
-
-    infoContainer.addEventListener('click', function(ev) {
-        infoContainer.classList.add('hidden');
-    });
-    // The player's state
-    var player = {
-        x: 0,
-        y: 0,
-        sizeX: 30,
-        sizeY: 30
-    };
-
-    // Don't run the game when the tab isn't visible
-    window.addEventListener('focus', function() {
-        unpause();
-    });
-
-    window.addEventListener('blur', function() {
-        pause();
-    });
-
-    window.addEventListener('resize', resize);
-
-    //Initially resize the game canvas.
-    resize();
-    // Let's play this game!
-    reset();
-    var then = Date.now();
-    var running = true;
-    main();
-
-
-    // Functions ---
-
-
-    // Reset game to original state
-    function reset() {
-        player.x = 0;
-        player.y = 0;
-    }
-
-    // Pause and unpause
-    function pause() {
-        running = false;
-    }
-
-    function unpause() {
-        running = true;
-        then = Date.now();
-        main();
-    }
-
-    // Update game objects.
-    // We'll use GameInput to detect which keys are down.
-    // If you look at the bottom of index.html, we load GameInput
-    // from js/input.js right before app.js
-    function update(dt) {
-        // Speed in pixels per second
-        var playerSpeed = 100;
-
-        if(GameInput.isDown('DOWN')) {
-            // dt is the number of seconds passed, so multiplying by
-            // the speed gives you the number of pixels to move
-            player.y += playerSpeed * dt;
-        }
-
-        if(GameInput.isDown('UP')) {
-            player.y -= playerSpeed * dt;
-        }
-
-        if(GameInput.isDown('LEFT')) {
-            player.x -= playerSpeed * dt;
-        }
-
-        if(GameInput.isDown('RIGHT')) {
-            player.x += playerSpeed * dt;
-        }
-
-        // You can pass any letter to `isDown`, in addition to DOWN,
-        // UP, LEFT, RIGHT, and SPACE:
-        // if(GameInput.isDown('a')) { ... }
-    }
-
-    // Draw everything
-    function render() {
-        ctx.fillStyle = 'black';
-        ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-        ctx.fillStyle = 'green';
-        ctx.fillRect(player.x, player.y, player.sizeX, player.sizeY);
-    }
-
-    // The main game loop
-    function main() {
-        if(!running) {
-            return;
-        }
-
-        var now = Date.now();
-        var dt = (now - then) / 1000.0;
-
-        update(dt);
-        render();
-
-        then = now;
-        requestAnimFrame(main);
-    }
-
-    // based on: https://hacks.mozilla.org/2013/05/optimizing-your-javascript-game-for-firefox-os/
-    function resize() {
-        var browser = [
-            window.innerWidth,
-            window.innerHeight
-        ];
-        // Minimum scale
-        var scale = Math.min(
-            browser[0] / initialCanvasWidth,
-            browser[1] / initialCanvasHeight);
-        // Scaled content size
-        var size = [
-            initialCanvasWidth * scale,
-            initialCanvasHeight * scale
-        ];
-        // Offset from top/left
-        var offset = [
-            (browser[0] - size[0]) / 2,
-            (browser[1] - size[1]) / 2
-        ];
-
-        // Apply CSS transform
-        var rule = "translate(" + offset[0] + "px, " + offset[1] + "px) scale(" + scale + ")";
-        mainContainer.style.transform = rule;
-        mainContainer.style.webkitTransform = rule;
-    }
-
-};
+function update() {
+}
