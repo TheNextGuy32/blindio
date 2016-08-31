995a60d6c75c2d07d3674bb0426349ff6b48b471
Ryan Mignini <rxm2995@rit.edu>
2016-08-31 13:36:36 -0400

    Add basic player, platforms
    Arrow keys move. Collides with walls, screen bounds.
    Todo: Rotation to face mouse, knife-throwing on click, lots more stuff.

--- js/app.js
+++ js/app.js
@@ -1,40 +1,66 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+var player, cursors, walls;
 
 function preload() {
-    // game.load.image('sky', 'assets/sky.png');
+    game.load.image('skyBackground', 'assets/sky.png');
+    game.load.image('playerSprite', 'assets/player.png');
+    game.load.image('wallSprite', 'assets/wall.png');
 }
 
 function create() {
     
     game.physics.startSystem(Phaser.Physics.ARCADE);
 
-    //  A simple background for our game
-    game.add.sprite(0, 0, 'sky');
+	//Adding things to world
+	//Stuff is rendered in the order it's added (back to front)
+	//Desired order: Background < Knives (to be added later) < Wind (later) < Players < Walls
+	
+    game.add.sprite(0, 0, 'skyBackground');
 
-    //  The platforms group contains the ground and the 2 ledges we can jump on
-    platforms = game.add.group();
+	player = game.add.sprite(game.world.width/2, game.world.height/2, 'playerSprite');
+	game.physics.enable(player);
+	player.body.gravity.y = 0;
+	player.body.collideWorldBounds = true;
 
-    //  We will enable physics for any object that is created in this group
-    platforms.enableBody = true;
 
-    // Here we create the ground.
-    var ground = platforms.create(0, game.world.height - 64, 'ground');
-
-    //  Scale it to fit the width of the game (the original sprite is 400x32 in size)
-    ground.scale.setTo(2, 2);
-
-    //  This stops it from falling away when you jump on it
-    ground.body.immovable = true;
-
-    //  Now let's create two ledges
-    var ledge = platforms.create(400, 400, 'ground');
-
-    ledge.body.immovable = true;
-
-    ledge = platforms.create(-150, 250, 'ground');
-
-    ledge.body.immovable = true;
+	walls = game.add.group();
+	walls.enableBody = true;
+	
+	//Wallsprite is 50px*50px, scale is multiplicative
+	var newWall = walls.create(game.world.width/4-25, game.world.height/4-25, 'wallSprite');
+	newWall.scale.setTo(2, 1);
+	newWall.body.immovable = true;
+	
+	newWall = walls.create(2*game.world.width/3, 3*game.world.height/4-75, 'wallSprite');
+	newWall.scale.setTo(1, 3);
+	newWall.body.immovable = true;
+	
+	
+	//Todo in the future: Change arrow keys to WASD
+    cursors = game.input.keyboard.createCursorKeys();
 }
 
 function update() {
+	game.physics.arcade.collide(player, walls);
+	
+	player.body.velocity.x = 0;
+	player.body.velocity.y = 0;
+	var moveSpeed = 300;
+	
+	if(cursors.left.isDown)
+	{
+		player.body.velocity.x -= moveSpeed;
+	}
+	if(cursors.right.isDown)
+	{
+		player.body.velocity.x += moveSpeed;
+	}
+	if(cursors.up.isDown)
+	{
+		player.body.velocity.y -= moveSpeed;
+	}
+	if(cursors.down.isDown)
+	{
+		player.body.velocity.y += moveSpeed;
+	}
 }
