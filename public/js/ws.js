b4651ec1ab5ecfa3c3dd5fb9825db7510c76c7dc
Oliver <oliverbarnum32@gmail.com>
2016-09-07 18:03:30 -0400

    Use secure websocket

--- public/js/ws.js
+++ public/js/ws.js
@@ -1,5 +1,5 @@
 window.onload = function(){
-  var ws       = new WebSocket('ws://' + window.location.host + window.location.pathname);
+  var ws       = new WebSocket('wss://' + window.location.host + window.location.pathname);
   ws.onopen    = function()  { console.log('websocket opened'); };
   ws.onclose   = function()  { console.log('websocket closed'); }
   ws.onmessage = function(m) { console.log('websocket message: ' +  m.data); };
