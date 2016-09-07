9da85c487550520bc92fd710abe050e48d313cc4
Oliver <oliverbarnum32@gmail.com>
2016-09-07 16:17:35 -0400

    Add client socket demo

--- app.rb
+++ app.rb
@@ -1,8 +1,26 @@
 require 'sinatra'
+require 'sinatra-websocket'
+
+set :sockets, []
 
 get '/' do
-	File.read(File.join('public', 'index.html'))
+  if !request.websocket?
+    File.read(File.join('public', 'index.html'))
+  else
+    request.websocket do |ws|
+     
+     ws.onopen do
+        ws.send("Hello World!")
+        settings.sockets << ws
+      end
+      ws.onmessage do |msg|
+        EM.next_tick { settings.sockets.each{|s| s.send(msg) } }
+      end
+      ws.onclose do
+        warn("websocket closed")
+        settings.sockets.delete(ws)
+      end
+    end
+
+  end
 end
-
-
-
