d2488bfd19c351613a1d63cf58f6281f0cbea5e9
Oliver <oliverbarnum32@gmail.com>
2016-11-19 17:29:09 -0500

    Remove redis

--- src/server.js
+++ src/server.js
@@ -1,28 +1,17 @@
 const express = require('express');
 const path = require('path');
-const redis = require('redis');
 
-// var redisClient = redis.createClient();
+const app = express();
+const server = require('http').Server(app);
+const io = require('./gameserver.js').listen(server);
 
-// redisClient.on('connect', function(err) {
-//   if (err) {
-//     console.log(err);
-//     throw err;
-//   }
-//     console.log('Connected to Redis server.');
+app.use(express.static(path.resolve(`${__dirname}/../public`)));
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(`${__dirname}/../public/index.html`));
+});
 
-  const app = express();
-  const server = require('http').Server(app);
-  const io = require('./gameserver.js').listen(server);
-
-  app.use(express.static(path.resolve(`${__dirname}/../public`)));
-  app.get('/', (req, res) => {
-    res.sendFile(path.resolve(`${__dirname}/../public/index.html`));
-  });
-
-  server.listen(Number(process.env.PORT || 5000));
-  console.log(`**************************`);
-  console.log(`* Listening on port ${process.env.PORT || 5000} *`);
-  console.log(`**************************`);
-//});
+server.listen(Number(process.env.PORT || 5000));
+console.log(`**************************`);
+console.log(`* Listening on port ${process.env.PORT || 5000} *`);
+console.log(`**************************`);
 
